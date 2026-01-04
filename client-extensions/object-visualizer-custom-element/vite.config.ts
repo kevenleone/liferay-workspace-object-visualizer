6a827260e8bb24f3cbe4c53d19823ff885fca758
Keven Leone <keven.santos.sz@gmail.com>
2026-01-04 19:12:05 -0300

    Add tauri settings

--- client-extensions/object-visualizer-custom-element/vite.config.ts
+++ client-extensions/object-visualizer-custom-element/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
 
+const host = process.env.TAURI_DEV_HOST;
+const tauriBuild = process.env.TAURI_BUILD;
+
 const devPort = 5173;
 
 export default defineConfig({
@@ -16,10 +19,23 @@ export default defineConfig({
                 entryFileNames: 'main.js',
             },
         },
+
+        target:
+            process.env.TAURI_ENV_PLATFORM == 'windows'
+                ? 'chrome105'
+                : 'safari13',
+        // don't minify for debug builds
+        minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+        // produce sourcemaps for debug builds
+        sourcemap: !!process.env.TAURI_ENV_DEBUG,
     },
+    clearScreen: false,
+    envPrefix: ['VITE_', 'TAURI_ENV_*'],
     experimental: {
         renderBuiltUrl(filename: string) {
-            return `/o/object-visualizer-custom-element/${filename}`;
+            return tauriBuild
+                ? filename
+                : `/o/object-visualizer-custom-element/${filename}`;
         },
     },
     resolve: {
@@ -30,6 +46,19 @@ export default defineConfig({
     server: {
         port: devPort,
         origin: `http://localhost:${devPort}`,
+        strictPort: true,
+        host: host || false,
+        hmr: host
+            ? {
+                  protocol: 'ws',
+                  host,
+                  port: 1421,
+              }
+            : undefined,
+
+        watch: {
+            ignored: ['**/src-tauri/**'],
+        },
     },
     plugins: [
         tanstackRouter({
